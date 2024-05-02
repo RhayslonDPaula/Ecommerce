6f9bb0179b684b94398b7f53fb4e5cc912f6887f
RHAYSLON MATHEUS DE PAULA CUNHA <37088883+RhayslonDPaula@users.noreply.github.com>
2024-05-02 10:19:45 -0300

    Nav Ped
    start of construction of the nav ped

--- rockstore/src/App.js
+++ rockstore/src/App.js
@@ -1,22 +1,54 @@
-import { Link, NavLink } from "react-router-dom";
+import { faBars, faSearch, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link,} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-  <div className="App">
-      <div className="nav">
-        <h1 className="logo">
-          Rock <span>STORE</span>
-        </h1>
-        <nav>
-          <ul>
-            <li> 
-              <Link to= "/">Home</Link>
-            </li>
+    <Router>   
+      <div className="App">
+        <div className="nav">
+         <div className="inner-content">
+          <h1 className="logo">
+            ROCK<span>STORE</span>
+          </h1>
+          <nav>
+            <ul>
+              <li> 
+                <Link to= "/">Home</Link>
+              </li>
+              <li> 
+                <Link to= "/products">Produtos</Link>
+              </li>
+              <li> 
+                <Link to= "/about">Sobre</Link>
+              </li>
+              <li> 
+                <Link to= "/contact">Contatos</Link>
+              </li>
+              <li> 
+                <Link to= "/account">conta</Link>
+              </li>
+            </ul>
+          </nav>
+          <div className="navs-icon-container">
+            <div className="search-input-container">
+              <input type="search" />
+              <FontAwesomeIcon icon={faSearch}/>
+            </div>
+            <button className="shopping-cart">
+              <FontAwesomeIcon icon={faShoppingCart} />
+              <div className="products-count">15</div>
+            </button>
+            <button className="menu-button">
+              <FontAwesomeIcon icon={faBars}/>
+            </button>
 
-          </ul>
-        </nav>
+          </div>
+         </div>
+        </div>
       </div>
-  </div>
-);
+    </Router>
+  );
 }
 export default App;
